f2301ea1b01809b266cef97a9223abad87824f87
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2013-10-16 03:27:20 -0700

    added simple stream for term movie

--- lib/twilm_stream.rb
+++ lib/twilm_stream.rb
@@ -1,5 +1,19 @@
-require "twilm_stream/version"
+# require 'twilm_stream/version'
+require 'rubygems'
+require 'time'
+require 'tweetstream'
+require 'mongo'
 
-module TwilmStream
-  # Your code goes here...
+TweetStream.configure do |config|
+	config.consumer_key = 'abcdefghij'
+	config.consumer_secret = '0123456789'
+	config.oauth_token = 'abcdefghij'
+	config.oauth_token_secret = '0123456789'
+	config.auth_method = :oauth
+end
+
+TweetStream::Client.new.on_error do |message|
+  p message
+end.track("movie") do |status|
+  p status.text
 end
